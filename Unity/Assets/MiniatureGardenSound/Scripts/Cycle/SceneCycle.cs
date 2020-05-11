204233c600d4803fe3e239e0395583aeac90ed8f
MizoTake <mizotake123@gmail.com>
2020-05-11 21:00:36 +0900

    adding scenecycle proto

---
+++ Unity/Assets/MiniatureGardenSound/Scripts/Cycle/SceneCycle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace MiniatureGardenSound.Cycle
+{
+    public class SceneCycle : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // todo: transition処理
+        }
+
+        private void OnDestroy()
+        {
+            // transition抜け処理？ 非同期なら問題ないはず
+        }
+    }
+}
